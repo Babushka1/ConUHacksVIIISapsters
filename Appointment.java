67b87a9d219f05be866cf045bfa0cb23b6b5d8bf
Pamela Daniel <149453946+bypameladaniel@users.noreply.github.com>
2024-01-20 17:22:02 -0500

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -21,4 +21,23 @@ public class Appointment{
         int[] array = {year, month, day};
         return array;
     }
+     public Date Conversion(String apptRequestDate){
+        int trait1 = apptRequestDate.indexOf('-');
+        int trait2 = apptRequestDate.lastIndexOf('-');
+
+        int year = Integer.parseInt(apptRequestDate.substring(0, trait1));
+        int month = Integer.parseInt(apptRequestDate.substring(trait1 + 1, trait2));
+        int day = Integer.parseInt(apptRequestDate.substring(trait2 + 1, 10));
+        int hour = Integer.parseInt(apptRequestDate.substring(11, 13));
+        int min = Integer.parseInt(apptRequestDate.substring(14));
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(year , month , day, hour, min);
+
+        Date date1 = calendar.getTime();
+
+        return date1;
+    }
+
 }
