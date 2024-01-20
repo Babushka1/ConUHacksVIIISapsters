61cc7e10953671e6c3402e83a43c60b7df5211c6
enyc24 <157230167+enyc24@users.noreply.github.com>
2024-01-20 17:33:37 -0500

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -7,7 +7,7 @@ public class Appointment{
     public Appointment(String apptRequestDate, String apptDate, String car){
         this.apptRequestDate = conversion(apptRequestDate);
         this.apptDate = conversion(apptDate);
-        this.car = car;
+        this.car = new Car(carType(car));
     }
 
      public Date conversion(String apptRequestDate){
@@ -28,5 +28,33 @@ public class Appointment{
 
         return date1;
     }
+
+       	public int carType(String car)
+   	{
+   		if (car.equals(" compact"))
+   		{
+   			return 0;
+   		}
+   		else if (car.equals(" medium"))
+   		{
+   			return 1;
+   		}
+   		else if (car.equals(" full-size"))
+   		{
+   			return 2;
+   		}
+   		else if (car.equals(" class 1 truck"))
+   		{
+   			return 3;
+   		}
+   		else if (car.equals(" class 2 truck"))
+   		{
+   			return 4;
+   		}
+   		else
+   		{
+   			return -1;
+   		}
+   	}
 
 }
