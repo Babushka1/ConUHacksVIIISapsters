677b9f351d2ebc3f641babb70d7a9025b3359b83
Pamela Daniel <149453946+bypameladaniel@users.noreply.github.com>
2024-01-20 17:37:58 -0500

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -10,19 +10,17 @@ public class Appointment{
         this.car = new Car(carType(car));
     }
 
-     public Date conversion(String apptRequestDate){
-        int trait1 = apptRequestDate.indexOf('-');
-        int trait2 = apptRequestDate.lastIndexOf('-');
+    public Date Conversion(String apptRequestDate){
 
-        int year = Integer.parseInt(apptRequestDate.substring(0, trait1));
-        int month = Integer.parseInt(apptRequestDate.substring(trait1 + 1, trait2));
-        int day = Integer.parseInt(apptRequestDate.substring(trait2 + 1, 10));
+        int year = Integer.parseInt(apptRequestDate.substring(0, 4));
+        int month = Integer.parseInt(apptRequestDate.substring(5, 7));
+        int day = Integer.parseInt(apptRequestDate.substring(8, 10));
         int hour = Integer.parseInt(apptRequestDate.substring(11, 13));
         int min = Integer.parseInt(apptRequestDate.substring(14));
 
         Calendar calendar = Calendar.getInstance();
 
-        calendar.set(year , month , day, hour, min);
+        calendar.set(year ,month ,day,hour,min);
 
         Date date1 = calendar.getTime();
 
