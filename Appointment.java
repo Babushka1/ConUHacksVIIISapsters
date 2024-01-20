cd91469dd282f7f66c41fffb0327f492b369ff11
jGavranovic <150096395+jGavranovic@users.noreply.github.com>
2024-01-20 17:24:32 -0500

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -5,23 +5,12 @@ public class Appointment{
     private Car car;
   
     public Appointment(String apptRequestDate, String apptDate, String car){
-        this.apptRequestDate = apptRequestDate;
-        this.apptDate = apptDate;
+        this.apptRequestDate = conversion(apptRequestDate);
+        this.apptDate = conversion(apptDate);
         this.car = car;
     }
 
-   public int[] Conversion(String apptRequestDate){
-        int trait1 = apptRequestDate.indexOf('-');
-        int trait2 = apptRequestDate.lastIndexOf('-');
-
-        int year = Integer.parseInt(apptRequestDate.substring(0, trait1));
-        int month = Integer.parseInt(apptRequestDate.substring(trait1 + 1, trait2));
-        int day = Integer.parseInt(apptRequestDate.substring(trait2 + 1));
-        
-        int[] array = {year, month, day};
-        return array;
-    }
-     public Date Conversion(String apptRequestDate){
+     public Date conversion(String apptRequestDate){
         int trait1 = apptRequestDate.indexOf('-');
         int trait2 = apptRequestDate.lastIndexOf('-');
 
