8f4a0fdc53a4fb0086da748ac1b0f119461961fa
jGavranovic <150096395+jGavranovic@users.noreply.github.com>
2024-01-20 16:39:46 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
@@ -11,7 +12,7 @@ public class Main {
         Appointment[] appointments = new Appointment[10000];
 
 
-        for (int i=0; i<10000; i++){
+        /*for (int i=0; i<10000; i++){
             String currentLine = scan.nextLine();
             appointments[i] = new Appointment(currentLine.substring(0,currentLine.indexOf(",")),currentLine.substring(currentLine.indexOf(",")+1,currentLine.lastIndexOf(",")),currentLine.substring(currentLine.lastIndexOf(",")+1));
         }
@@ -19,7 +20,20 @@ public class Main {
 
         for (int i=0;i<10000;i++){
             System.out.println(appointments[i]);
-        }
+        }*/
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(2023, 0, 8, 0, 0, 0);
+
+        Date date1 = calendar.getTime();
+
+        calendar.set(2024, 0, 20, 0, 0, 0);
+
+        Date date2 = calendar.getTime();
+
+        System.out.println(date1);
+        System.out.println(date2);
 
 
         scan.close();
