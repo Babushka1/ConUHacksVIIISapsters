c8a7188878f02abc8490372d3f084e8b9b566d97
enyc24 <157230167+enyc24@users.noreply.github.com>
2024-01-20 21:58:31 -0500

    Update Schedule.java

--- Schedule.java
+++ Schedule.java
@@ -1,6 +1,5 @@
 
 public class Schedule {
-	
 	public static Bay [][] baySchedule = new Bay[61][10];
 	
 	public void baySchedule()
@@ -8,15 +7,16 @@ public class Schedule {
 		for (int i = 0; i < baySchedule.length; i++)
 		{
 			for (int j = i; j < baySchedule.length; j++)
-			if (i > 4)
+			if (j > 4)
 			{
-				baySchedule [i][j] = new Bay();
+				baySchedule [i][j] = new Bay(5);
 			}
 			else
 			{
-				baySchedule[i][j] = new Bay();
+				baySchedule[i][j] = new Bay(j);
 			}
 		}
+    }
     }
 
 }
