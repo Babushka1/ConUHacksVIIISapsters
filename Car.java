1f6354503ffb5277252e61f3f70378b0b4798d86
jGavranovic <150096395+jGavranovic@users.noreply.github.com>
2024-01-21 01:49:56 -0500

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,4 +1,3 @@
-
 public class Car {
 	
 	final int COMPACT = 0;
@@ -8,23 +7,20 @@ public class Car {
 	final int CLASS2 = 4;
 	
 	int type;
-	public final int [] serviceTime = {30, 30, 30, 60, 120};
-	public final int [] servicePrice = {150, 150, 150, 250, 700};	
+	public final static int [] serviceTime = {30, 30, 30, 60, 120};
+	public final static int [] servicePrice = {150, 150, 150, 250, 700};	
 	
-	
-	
-	public int getType() {
-		return type;
-	}
-
-	public void setType(int type) {
-		this.type = type;
-	}
-
 	public Car (int num)
 	{
 		type = num;
 	}
 	
+    public String toString(){
+        return ""+type;
+    }
+
+    public int getType(){
+        return type;
+    }
 	
 }
