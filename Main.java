4b644cad433e3d069a6df570e0a9bca59d9bf102
jGavranovic <150096395+jGavranovic@users.noreply.github.com>
2024-01-20 20:19:25 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,6 +10,13 @@ public class Main {
         Scanner scan = new Scanner(file);
 
         Appointment[] appointments = new Appointment[10000];
+        Bay[] bays = new Bay[10];
+        
+        for (int i=0; i<bays.length; i++){
+            if (i>4) bays[i]=new Bay(0);
+
+            else bays[i]= new Bay(i);
+        }
 
 
         for (int i=0; i<10000; i++){
@@ -22,8 +29,40 @@ public class Main {
             System.out.println(appointments[i].apptRequestDate+" ||| "+appointments[i].apptDate+" ||| Type "+appointments[i].car);
         }
 
+        for (int i=0;i<appointments.length; i++){
+
+        }
         
 
+
         scan.close();
+    }
+    static void swap(Appointment[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    static int partition(Appointment[] array, int low, int high)
+    {
+        // Choosing the pivot
+        int pivot = arr[high];
+ 
+        // Index of smaller element and indicates
+        // the right position of pivot found so far
+        int i = (low - 1);
+ 
+        for (int j = low; j <= high - 1; j++) {
+ 
+            // If current element is smaller than the pivot
+            if (array[j].getApptRequestDate().compareTo(array[pivot].getApptRequestDate())) {
+ 
+                // Increment index of smaller element
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
     }
 }
