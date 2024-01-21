26dcf3eb93e349c9edc48b46dbb040c23e9e0d3b
jGavranovic <150096395+jGavranovic@users.noreply.github.com>
2024-01-20 21:56:12 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,68 +1,75 @@
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.io.NoSuchElementException;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new  File("C:\\Users\\jovro\\Desktop\\SAP\\datafile.csv");
+        File file = new  File("C:\\Users\\jovro\\Desktop\\SAP\\unsorteddatafile.csv");
         Scanner scan = new Scanner(file);
 
-        Appointment[] appointments = new Appointment[10000];
-        Bay[] bays = new Bay[10];
-        
-        for (int i=0; i<bays.length; i++){
-            if (i>4) bays[i]=new Bay(5);
-
-            else bays[i]= new Bay(i);
+        int lines = 0;
+        try {
+            while (true) {
+                scan.nextLine();
+                lines++;
+            }
+        } catch (java.util.NoSuchElementException e){
+            
         }
 
+        scan = new Scanner(file);
 
-        for (int i=0; i<10000; i++){
+        Appointment[] appointments = new Appointment[lines];
+
+
+        for (int i=0; i<lines; i++){
             String currentLine = scan.nextLine();
             appointments[i] = new Appointment(currentLine.substring(0,currentLine.indexOf(",")),currentLine.substring(currentLine.indexOf(",")+1,currentLine.lastIndexOf(",")),currentLine.substring(currentLine.lastIndexOf(",")+1));
         }
-
-
-        for (int i=0;i<10000;i++){
-            System.out.println(appointments[i].apptRequestDate+" ||| "+appointments[i].apptDate+" ||| Type "+appointments[i].car);
-        }
-
-        for (int i=0;i<appointments.length; i++){
-
-        }
         
+        quickSort(appointments, 0, lines-1);
 
+        for (int i=0;i<lines;i++){
+            System.out.println(appointments[i].getApptRequestDate());
+        }
 
-        scan.close();
+        //scan.close();
     }
     static void swap(Appointment[] array, int i, int j)
     {
-        int temp = array[i];
+        Appointment temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
     static int partition(Appointment[] array, int low, int high)
     {
-        // Choosing the pivot
-        int pivot = arr[high];
- 
-        // Index of smaller element and indicates
-        // the right position of pivot found so far
+
+        Date pivot = array[high].getApptRequestDate();
+
         int i = (low - 1);
  
         for (int j = low; j <= high - 1; j++) {
  
-            // If current element is smaller than the pivot
-            if (array[j].getApptRequestDate().compareTo(array[pivot].getApptRequestDate())) {
+            if (array[j].getApptRequestDate().compareTo(pivot)<0) {
  
-                // Increment index of smaller element
                 i++;
                 swap(array, i, j);
             }
         }
-        swap(arr, i + 1, high);
+        swap(array, i + 1, high);
         return (i + 1);
+    }
+    static void quickSort(Appointment[] array, int low, int high)
+    {
+        if (low < high) {
+ 
+            int pi = partition(array, low, high);
+ 
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
     }
 }
